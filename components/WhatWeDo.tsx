63bbd5d34e838dda45df945e7fbc871a06b58a19
midqc <concettina6069@rustyload.com>
2024-10-14 15:57:16 +0530

    updated choose us images

--- components/WhatWeDo.tsx
+++ components/WhatWeDo.tsx
@@ -79,9 +79,9 @@ export default function WhatWeDo() {
             <Image
               src={feature.image}
               alt={`${feature.title} icon`}
-              width={96}
-              height={96}
-              className="w-24 h-24 mb-4 object-contain"
+              width={64}
+              height={64}
+              className="mb-4 object-contain"
             />
             <h3 className="text-xl font-medium mb-2 text-blue-600 text-center">
               {feature.title}
